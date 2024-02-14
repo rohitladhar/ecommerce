364a8fa461fbbaeeb68607c1e7d4fbd04d3fec6a
Rohini Ladhar <rohit.ladhar10@gmail.com>
2024-02-14 15:57:15 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+ng add @angular/material
+ng add @ngrx/store
+ng add @ngrx/store-devtools
+ng add @ngrx/router-store
+ng add @ngrx/effects
---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule, isDevMode } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { EffectsModule } from '@ngrx/effects';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    StoreModule.forRoot({}, {}),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
+    StoreRouterConnectingModule.forRoot(),
+    EffectsModule.forRoot([])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
